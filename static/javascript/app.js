a574d58a4f6cb373651a56d7aae86702cfbc807b
Cory Morrissey <corydm92@gmail.com>
2018-12-27 14:49:13 -0800

    initial push

---
+++ static/javascript/app.js
@@ -0,0 +1,51 @@
+// let menuButton = document.getElementById('menu-img');
+
+let navContainer = document.getElementById('nav-container')
+
+let headerContainer = document.getElementById('header-container');
+
+let hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener('click', function(e) {
+
+    if (hamburger.classList.contains('unclicked')) {
+        hamburger.classList.toggle("is-active");
+        hamburger.classList.remove('unclicked');
+        hamburger.classList.add('clicked');
+
+        navContainer.classList.add('show');
+    } else {
+        hamburger.classList.toggle("is-active");
+        hamburger.classList.remove('clicked');
+        hamburger.classList.add('unclicked');
+
+        navContainer.classList.remove('show');
+    }
+
+});
+
+
+// Javascript to link to About in page
+let aboutLink = document.getElementById('about-button')
+
+aboutLink.addEventListener('click', function(e) {
+    let projectElement = document.getElementById('about-section');
+
+    projectElement.scrollIntoView(true);
+    
+    hamburger.click();
+
+});
+
+// Javascript to link to Projects in page
+let projectsLink = document.getElementById('project-button')
+
+projectsLink.addEventListener('click', function(e) {
+    let projectElement = document.getElementById('project-link');
+
+    projectElement.scrollIntoView(true);
+    
+    hamburger.click();
+
+});
+
